99dd73b52c662f56ab59169d07a2e9b5e01b36f9
Chitaoji <2360742040@qq.com>
2025-04-11 01:08:48 +0800

    Add contact_book()

--- examples/test_data.py
+++ examples/test_data.py
@@ -2,6 +2,8 @@
 
 from typing import TYPE_CHECKING
 
+from faker import Faker
+
 from ..src import config
 
 if TYPE_CHECKING:
@@ -11,6 +13,33 @@ if TYPE_CHECKING:
 __all__ = ["contact_book"]
 
 
-def contact_book() -> "ConfigIOWrapper":
-    """Returns a fake contact book."""
-    return config(None)
+def contact_book(number_of_people: int = 20) -> "ConfigIOWrapper":
+    """
+    Returns a fake contact book.
+
+    Parameters
+    ----------
+    number_of_people : int, optional
+        Number of people on the contact book, by default 20.
+
+    Returns
+    -------
+    ConfigIOWrapper
+        Config object.
+
+    """
+    faker = Faker()
+    book = {}
+    for _ in range(number_of_people):
+        name = faker.name()
+        book[name] = {
+            "name": name,
+            "email": faker.email(),
+            "phone_number": faker.phone_number(),
+            "address": [faker.city()] + faker.address().splitlines(),
+            "mac_addresses": {
+                faker.port_number(): faker.mac_address()
+                for __ in range(faker.pyint(min_value=1, max_value=10))
+            },
+        }
+    return config(book)
