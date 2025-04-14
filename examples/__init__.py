8a48bc6a5e4a1921b00b7b598635f725a784dbea
Chitaoji <2360742040@qq.com>
2025-04-14 19:39:34 +0800

    Use lazyr

--- examples/__init__.py
+++ examples/__init__.py
@@ -4,7 +4,7 @@ import lazyr
 
 VERBOSE = 3
 
-lazyr.register("faker", verbose=VERBOSE)
+lazyr.register("faker", ignore=["Faker"], verbose=VERBOSE)
 
 # pylint: disable=wrong-import-position
 from . import test_case
